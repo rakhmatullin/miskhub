c8a3471705530114db4947ec60574dae7766e3e4
Renat Rakhmatullin <38514966+rakhmatullin@users.noreply.github.com>
2026-09-17 16:25:20 +0300

    bust cloud.js cache

--- akty.js
+++ akty.js
@@ -5,7 +5,7 @@
       .then(function(txt){ var s=document.createElement('script'); s.textContent=txt; document.body.appendChild(s); });
   }
   boot('./akty.engine.js?v=4')
-    .then(function(){ return boot('./akty.cloud.js?v=5'); })
+    .then(function(){ return boot('./akty.cloud.js?v=6'); })
     .catch(function(e){
       console.error(e);
       document.body.insertAdjacentHTML('beforeend','<p style="color:#ff6b7a;padding:20px">Движок не загрузился: '+e+'</p>');
